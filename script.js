7bc5c8289b6b27220e1ef75956d64ad9627a464c
Ermianr <ermianrazor@gmail.com>
2024-10-23 18:14:34 -0500

    Se añadieron funciones graficas modifcando el DOM

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
-let humanScore = 0
-let computerScore = 0
+const buttons = document.querySelectorAll("button");
+const resultados = document.querySelector("#resultado");
+const movimientoCpu = document.querySelector("#movimiento-cpu");
+const estadoVs = document.querySelector("#estado-vs");
+let finalHumanChoice = "";
+let humanScore = 0;
+let computerScore = 0;
+let games = 0;
+
+buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        if (e.target.id === "piedra") {
+            finalHumanChoice = "piedra";
+            games++;
+            playGame();
+        } else if (e.target.id === "papel") {
+            finalHumanChoice = "papel";
+            games++;
+            playGame();
+        } else if (e.target.id === "tijeras") {
+            finalHumanChoice = "tijeras";
+            games++;
+            playGame();
+        } else {
+            reset();
+        }
+    })
+})
+
 
 const getComputerChoice = () => {
     const randNumber = Math.round(Math.random() * 2)
@@ -8,58 +35,54 @@ const getComputerChoice = () => {
     return computerChoice
 }
 
-const getHumanChocie = () => {
-    const humanChoice = prompt("Ingrese 'Piedra' 'Papel 'Tijeras'")
-    return humanChoice
-}
-
 const playRound = (humanChoice, computerChoice) => {
-    humanChoice = humanChoice.toLowerCase()
     if (humanChoice === computerChoice) {
-        console.log(`CPU Escoge ${computerChoice}` )
-        console.log("¡Empate!")
+        movimientoCpu.innerText = `CPU Escoge ${computerChoice}`;
+        estadoVs.innerText = "¡Empate!";
     } else if (humanChoice === "tijeras" && computerChoice === "papel") {
-        console.log(`CPU Escoge ${computerChoice}` )
-        console.log("¡Ganas! Tijeras le gana a Papel")
-        humanScore++
+        movimientoCpu.innerText = `CPU Escoge ${computerChoice}`;
+        estadoVs.innerText = "¡Ganas! Tijeras le gana a Papel";
+        humanScore++;
     } else if (humanChoice === "tijeras" && computerChoice === "piedra") {
-        console.log(`CPU Escoge ${computerChoice}` )
-        console.log("¡Pierdes! Piedra le gana a Tijeras")
-        computerScore++
+        movimientoCpu.innerText = `CPU Escoge ${computerChoice}`;
+        estadoVs.innerText = "¡Pierdes! Piedra le gana a Tijeras";
+        computerScore++;
     } else if (humanChoice === "papel" && computerChoice === "piedra") {
-        console.log(`CPU Escoge ${computerChoice}` )
-        console.log("¡Ganas! Papel le gana a Piedra")
-        humanScore++
+        movimientoCpu.innerText = `CPU Escoge ${computerChoice}`;
+        estadoVs.innerText = "¡Ganas! Papel le gana a Piedra";
+        humanScore++;
     } else if (humanChoice === "papel" && computerChoice === "tijeras") {
-        console.log(`CPU Escoge ${computerChoice}` )
-        console.log("¡Pierdes! Tijeras le gana a Papel")
-        computerScore++
+        movimientoCpu.innerText = `CPU Escoge ${computerChoice}`;
+        estadoVs.innerText = "¡Pierdes! Tijeras le gana a Papel";
+        computerScore++;
     } else if (humanChoice === "piedra" && computerChoice === "papel") {
-        console.log(`CPU Escoge ${computerChoice}` )
-        console.log("¡Pierdes! Papel le gana a Piedra")
-        computerScore++
+        movimientoCpu.innerText = `CPU Escoge ${computerChoice}`;
+        estadoVs.innerText ="¡Pierdes! Papel le gana a Piedra";
+        computerScore++;
     } else if (humanChoice === "piedra" && computerChoice === "tijeras") {
-        console.log(`CPU Escoge ${computerChoice}` )
-        console.log("¡Ganas! Piedra le gana a Tijeras")
-        humanScore++
+        movimientoCpu.innerText = `CPU Escoge ${computerChoice}`;
+        estadoVs.innerText = "¡Ganas! Piedra le gana a Tijeras";
+        humanScore++;
     }     
 }
 
-const playGame = () => { 
-    let counter = 1
-    while (counter <= 5) {
-        const finalComputerChoice = getComputerChoice()
-        const finalHumanChoice = getHumanChocie()
-        playRound(finalHumanChoice, finalComputerChoice)
-        counter++
-    }
-    if (humanScore > computerScore) {
-        console.log(`Humano gano con ${humanScore} puntos.`)
-    } else {
-        console.log(`Maquina gano con ${computerScore} puntos.`)
+const playGame = () => {
+    const finalComputerChoice = getComputerChoice();
+    playRound(finalHumanChoice, finalComputerChoice);
+    if (games === 5) {
+        if (humanScore > computerScore) {
+            resultados.innerText = ` ---FIN DEL JUEGO--- Humano gano con ${humanScore} puntos.`;
+        } else {
+            resultados.innerText = (` ---FIN DEL JUEGO--- Maquina gano con ${computerScore} puntos.`);
+        }
     }
 }
 
-playGame()
-
-
+const reset = () => {
+    computerScore = 0;
+    humanScore = 0;
+    resultados.innerText = "";
+    movimientoCpu.innerText = "";
+    estadoVs.innerText = "";
+    games = 0;
+}
